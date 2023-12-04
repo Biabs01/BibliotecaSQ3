09afe138845352c140515bded420d17f537656ae
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-12-04 16:06:45 -0300

    modified:   App.js modified:   package.json new file:   screens/Search.js new file:   screens/Transactions.js modified:   yarn.lock

---
+++ screens/Transactions.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default class TransactionsScreen extends Component{
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.text}>Tela de Transações</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#5653D4'
+    },
+    text: {
+        color: '#FFFFFF',
+        fontSize: 30
+    },
+});
