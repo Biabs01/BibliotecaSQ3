3be16b0a1816876acbd0f7d734c29894524761cb
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2023-12-11 16:04:42 -0300

    modified:   screens/Transactions.js

--- screens/Transactions.js
+++ screens/Transactions.js
@@ -7,6 +7,8 @@ export default class TransactionsScreen extends Component{
     constructor(props){
         super(props);
         this.state = {
+            bookId: '',
+            studentId: '',
             domState: 'normal',
             hasCameraPermissions: null,
             scanned: false,
@@ -23,8 +25,34 @@ export default class TransactionsScreen extends Component{
         });
     }
 
+    handleBarCodeScanned = async ({type, data}) => {
+        const {domState} = this.state;
+
+        if(domState === 'bookId'){
+            this.setState({
+                bookId: data,
+                domState: 'normal',
+                scanned: true
+            });
+        } else if(domState === 'studentId'){
+            this.setState({
+                studentId: data,
+                domState: 'normal',
+                scanned: true
+            });
+        }
+    }
+
     render(){
-        const {domState, hasCameraPermissions, scanned, scannedData} = this.state;
+        const {bookId, studentId, domState, scanned} = this.state;
+        if(domState !== 'normal'){
+            return (
+                <BarCodeScanner
+                    onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
+                    style={StyleSheet.absoluteFillObject}
+                />
+            );
+        }
         return(
             <View style={styles.container}>
                 <Text style={styles.text}>
@@ -36,11 +64,27 @@ export default class TransactionsScreen extends Component{
                             style={styles.textinput}
                             placeholder={'Id do Livro'}
                             placeholderTextColor={"#FFFFFF"}
-                            //value={bookId}
+                            value={bookId}
                         />
                         <TouchableOpacity
                             style={styles.scanbutton}
-                            onPress={() => this.getCameraPermissions("scanner")}
+                            onPress={() => this.getCameraPermissions("bookId")}
+                        >
+                            <Text style={styles.scanbuttonText}>
+                                Scanear
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={[styles.textinputContainer, {marginTop: 25}]}>
+                        <TextInput
+                            style={styles.textinput}
+                            placeholder={'Id do Aluno'}
+                            placeholderTextColor={"#FFFFFF"}
+                            value={studentId}
+                        />
+                        <TouchableOpacity
+                            style={styles.scanbutton}
+                            onPress={() => this.getCameraPermissions("studentId")}
                         >
                             <Text style={styles.scanbuttonText}>
                                 Scanear
@@ -64,4 +108,39 @@ const styles = StyleSheet.create({
         color: '#FFFFFF',
         fontSize: 30
     },
+    lowerContainer: {
+        flex: 0.5,
+        alignSelf: 'center'
+    },
+    textinputContainer: {
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#FFFFFF',
+        flexDirection: 'row',
+        backgroundColor: '#9dfd24'
+    },
+    textinput: {
+        width: '75%',
+        height: 50,
+        padding: 10,
+        borderColor: '#FFFFFF',
+        borderRadius: 10,
+        borderWidth: 3,
+        fontSize: 18,
+        backgroundColor: "#5653d4",
+        color: '#FFFFFF'
+    },
+    scanbutton: {
+        width: 100,
+        height: 50,
+        backgroundColor: '#9dfd24',
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    scanbuttonText: {
+        fontSize: 20,
+        color: '#0A0101'
+    }
 });
