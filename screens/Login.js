f3baaadea4e9b330f5e5a64e31653c85b155e97b
Biabs01 <108082691+Biabs01@users.noreply.github.com>
2024-01-17 15:54:25 -0300

    new file:   assets/appIcon.png 	new file:   assets/appName.png 	new file:   assets/background1.png 	new file:   assets/background2.png 	modified:   screens/Login.js

--- screens/Login.js
+++ screens/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import {View, Text, StyleSheet, Image, TouchableOpacity, Alert, KeyboardAvoidingView, TextInput, ImageBackground} from 'react-native';
 
+const bgImage = require('../assets/background1.png');
+const appIcon = require('../assets/appIcon.png');
+const appName = require('../assets/appName.png');
+
 export default class Login extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    handleLogin = (email, password) => {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.props.navigation.navigate('BottomTab');
+            })
+            .catch(error => {
+                Alert.alert(error.message);
+            });
+    }
+
     render(){
+        const {email, password} = this.state;
         return(
             <KeyboardAvoidingView behavior='padding' style={styles.container}>
                 <ImageBackground source={bgImage} style={styles.bgImage}>
@@ -39,5 +64,56 @@ export default class Login extends React.Component{
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF'
+    },
+    bgImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center'
+    },
+    upperContainer: {
+        flex: 0.5,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    appIcon: {
+        width: 280,
+        height: 280,
+        resizeMode: 'contain',
+        marginTop: 80
+    },
+    appName: {
+        width: 130,
+        height: 130,
+        resizeMode: 'contain'
+    },
+    lowerContainer: {
+        flex: 0.5,
+        alignItems: 'center'
+    },
+    textinput:{
+        width: '75%',
+        height: 55,
+        padding: 10,
+        borderWidth: 4,
+        borderRadius: 10,
+        borderColor: '#FFFFFF',
+        fontSize: 18,
+        color: '#FFFFFF',
+        backgroundColor: '#5653d4'
+    },
+    button: {
+        width: '43%',
+        height: 55,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#9DFD24',
+        borderRadius: 15
+    },
+    scanbuttonText: {
+        fontSize: 24,
+        color: '#FFFFFF'
+    }
 })
